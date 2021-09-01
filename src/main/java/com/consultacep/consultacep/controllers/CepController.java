e4e643dc5a83a9783488b04effb6aafa1d8b0df0
lucas-salves <lucas.salves@terra.com.br>
2021-09-01 17:31:04 -0300

    add: adding the @RequestMapping e @GetMapping no CepController.java

--- src/main/java/com/consultacep/consultacep/controllers/CepController.java
+++ src/main/java/com/consultacep/consultacep/controllers/CepController.java
@@ -1,23 +1,25 @@
 package com.consultacep.consultacep.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.consultacep.consultacep.model.Cep;
 
 @RestController
+@RequestMapping("/consulta")
 public class CepController {
 	
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@GetMapping("/cep")
 	public Cep getCountries(){
 		String url = "https://viacep.com.br/ws/08081010/json/";
 		Cep cepObject = restTemplate.getForObject(url, Cep.class);
-		
+		System.out.println("Teste: "+cepObject);
 		return cepObject;
 	}
 	
