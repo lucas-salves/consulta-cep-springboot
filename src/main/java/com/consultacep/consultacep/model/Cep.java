e025a1c91f67a970f916668f18722b18d29eba33
lucas-salves <lucas.salves@terra.com.br>
2021-09-01 17:30:23 -0300

    add: creation of the Cep attributes

--- src/main/java/com/consultacep/consultacep/model/Cep.java
+++ src/main/java/com/consultacep/consultacep/model/Cep.java
@@ -1,5 +1,21 @@
 package com.consultacep.consultacep.model;
 
-public class Cep {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cep {
+	private String cep;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String localidade;
+	private String uf;
+	private String ibge;
+	private String gia;
+	private String ddd;
+	private String siafi;
 }
