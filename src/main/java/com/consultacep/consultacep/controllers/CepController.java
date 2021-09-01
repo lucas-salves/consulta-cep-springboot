d89e70b91b903a032ddcb7de96fc0d6e936cabfa
lucas-salves <lucas.salves@terra.com.br>
2021-09-01 16:54:21 -0300

    add: creaton of the method getCountries that will get and return  the RestTemplate response

--- src/main/java/com/consultacep/consultacep/controllers/CepController.java
+++ src/main/java/com/consultacep/consultacep/controllers/CepController.java
@@ -1,11 +1,24 @@
 package com.consultacep.consultacep.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.consultacep.consultacep.model.Cep;
 
 @RestController
 public class CepController {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+
+	public Cep getCountries(){
+		String url = "https://viacep.com.br/ws/08081010/json/";
+		Cep cepObject = restTemplate.getForObject(url, Cep.class);
+		
+		return cepObject;
+	}
 	
 }
